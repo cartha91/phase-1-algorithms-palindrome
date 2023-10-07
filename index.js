95a9dbc4704db2fa2a9650dfec2f50bc994d0b2d
steven dulworth <crowscoupetech@proton.me>
2023-10-07 12:31:05 -0500

    palindramitic lol

--- index.js
+++ index.js
@@ -1,18 +1,45 @@
-function isPalindrome(word) {
-  // Write your algorithm here
+  // 'isPalindrone' should take one argument,
+  // that argument should be a string,
+  // check entered string for palindrone,
+  // if string is palindrome return true,
+  // if string is not palindrone return false
+  
+  function isPalindrome(word) {
+  let changedWords = word.toLowerCase().replace(/[^a-z0-9]/gi, '');
+
+  let invertedWords = changedWords.split('').reverse().join('');
+
+  return changedWords === invertedWords;
 }
 
 /* 
-  Add your pseudocode here
+  change the entered word to lowercase and get rid of all characters that are not letters
+
+  invert the changed word
+
+  now check to see if the inverted word matches the original word
+
+  console.log true or false based on the entered string 
 */
 
 /*
-  Add written explanation of your solution here
+  first 'isPalindrome accepts an argument then proceeds to change that string into an all lowercase string as well as remove any characters that are not letters. 
+  .toLowerCase handles changing the case while .replace will handle the type of character. Next the function will make a new copy of our changed word, reverse it, then
+  check to see if the two words match. last we will see a return value of true if the word matched and false if it did not.
 */
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
-  // add your own custom tests in here
+  /*
+    describe('isPalindrome', function() {
+    it('should return true for "mom"', function() {
+        assert.isTrue(isPalindrome('abba'));
+    });
+
+  */
+ console.log("Expecting: true");
+ console.log("=>", isPalindrome("mom"));
+ 
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
